b1be46dc833c1edf882e4357e04b65ca43c8a0a2
Connor Peebles <connorpeebles93@gmail.com>
2018-09-04 20:30:33 +0000

    Adds the first assignment questions

---
+++ howManyHundreds.js
@@ -0,0 +1,11 @@
+function howManyHundreds(num) {
+  var remainder = num % 100;
+  var hundreds = (num - remainder) / 100;
+  return hundreds;
+}
+
+console.log(howManyHundreds(1000), "=?", 10);
+console.log(howManyHundreds(894), "=?", 8);
+console.log(howManyHundreds(520), "=?", 5);
+console.log(howManyHundreds(99), "=?", 0);
+console.log(howManyHundreds(0), "=?", 0);
