2f587b29ee9198624cebb8a6944796c36f989132
Connor Peebles <connorpeebles93@gmail.com>
2018-09-05 18:22:10 +0000

    Adds looping assignment questions, up to LoopyLighthouse

---
+++ lastIndexOf.js
@@ -0,0 +1,16 @@
+function lastIndexOf(anArray, aValue) {
+  var counter = anArray.length;
+  while (counter > 0) {
+    if (anArray[counter - 1] == aValue) {
+      return counter - 1;
+    }
+    counter--;
+  }
+  return -1;
+}
+
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 1), "=?", 3);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 2), "=?", 4);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 3), "=?", -1);
+console.log(lastIndexOf([ 5, 5, 5 ], 5), "=?", 2);
+console.log(lastIndexOf([], 3), "=?", -1);
---
+++ loopyLighthouse.js
@@ -0,0 +1,14 @@
+var counter = 100;
+
+while (counter < 201) {
+  if (counter % 3 == 0 && counter % 4 == 0) {
+    console.log("LoopyLighthouse");
+  } else if (counter % 3 == 0) {
+    console.log("Loopy");
+  } else if (counter % 4 == 0) {
+    console.log("Lighthouse");
+  } else {
+    console.log(counter)
+  }
+  counter++;
+}
